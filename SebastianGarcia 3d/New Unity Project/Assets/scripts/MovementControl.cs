ef36c75d0d1d2228fe41d52928c3831aa6f35173
Xebastiano <sebastiangarciacastaneda@gmail.com>
2019-04-13 23:30:55 -0500

    Archivos Actualizados

---
+++ SebastianGarcia 3d/New Unity Project/Assets/scripts/MovementControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementControl : MonoBehaviour {
+
+    public float speed = 1;
+    public float horizontalJumpDistance = 1;
+
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //Vector3 tempVector = Vector3.zero;
+        //tempVector.z = speed;
+        //transform.position += tempVector * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            Vector3 tempVector = Vector3.zero;
+            tempVector.z = speed;
+            transform.position += tempVector * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            Vector3 tempVector = Vector3.zero;
+            tempVector.x = -horizontalJumpDistance;
+            transform.position += tempVector;
+        } else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+            Vector3 tempVector = Vector3.zero;
+            tempVector.x = horizontalJumpDistance;
+            transform.position += tempVector;
+        }
+    }
+}
--- SebastianGarcia 3d/New Unity Project/Assets/scripts/MyClass.cs
+++ SebastianGarcia 3d/New Unity Project/Assets/scripts/MyClass.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MyClass : MonoBehaviour
-{
+public class MyClass : MonoBehaviour {
+
+    public int number = 5;
+    public Vector3 position;
+    public GameObject cubeObject;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start () {
+        GameObject testObject = new GameObject ();
+        testObject.AddComponent<MeshFilter> ().sharedMesh = cubeObject.GetComponent<MeshFilter> ().sharedMesh;
+        testObject.AddComponent<MeshRenderer> ();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update () {
+
     }
 }
