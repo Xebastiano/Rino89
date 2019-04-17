f9e068269f2e154a5bac6b22dcdb4b5571f02dd7
Xebastiano <sebastiangarciacastaneda@gmail.com>
2019-04-16 22:14:11 -0500

    NuevoUnity

---
+++ NuevoProyectoUnity/Assets/Scripts/ChangeMaterial.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterial : MonoBehaviour {
+
+    private MeshRenderer meshRenderer;
+    private Material oldMaterial;
+    public Material newMaterial;
+
+    // Start is called before the first frame update
+    void Start(){
+
+        meshRenderer = GetComponent<MeshRenderer> ();
+        oldMaterial = meshRenderer.material;
+    }
+
+    public void ChangeMaterialToNew(){
+        meshRenderer.material = newMaterial;
+    }
+
+    public void ChangeMaterialToOld()
+    {
+        meshRenderer.material = oldMaterial;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+}
---
+++ NuevoProyectoUnity/Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private bool canJump;
+
+    public float speed = 1f;
+    public float jumpForce = 10f;
+
+    // Start is called before the first frame update
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+        canJump = true;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        transform.position += new Vector3(h,0,v) * speed * Time.deltaTime;
+
+        if(Input.GetKeyDown(KeyCode.Space) && canJump){
+            canJump = false;
+            rb.AddForce(Vector3.up * jumpForce,ForceMode.Impulse);
+        }
+    }
+    void OnCollisionEnter (Collision collision){
+        if(collision.gameObject.tag == "Chonque")
+            canJump = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter");
+        other.GetComponent<ChangeMaterial>().ChangeMaterialToNew();
+    }
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("OnTriggerExit");
+        other.GetComponent<ChangeMaterial>().ChangeMaterialToOld();
+    }
+}
