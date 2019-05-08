10db28a11ffb6c82db7b04f45bba1f63fa9dc0eb
Xebastiano <sebastiangarciacastaneda@gmail.com>
2019-05-07 22:22:38 -0500

    Limites :v

---
+++ Sebastián García 2D/Assets/Scripts/CamMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMovement : MonoBehaviour {
+
+    public Transform followTarget;
+    public float followSpeed;
+    //Vector2 camUnitDimentions = new Vector2(8.9f,5.0f);
+    Vector2 camUnitDimentions;
+    Vector2 limits;
+
+    // Start is called before the first frame update
+    void Start () {
+
+        limits = new Vector2(10.0f,7.0f) - camUnitDimentions;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (followTarget) {
+            /*Vector3 temp = Vector3.MoveTowards (transform.position, followTarget.position, followSpeed * Time.deltaTime);
+            temp.z = transform.position.z;
+            transform.position = temp;*/
+            Vector3 direction = (followTarget.position - transform.position).normalized;
+
+        }
+    }
+}
---
+++ Sebastián García 2D/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float moveSpeed = 15;
+    public Vector2 limits = new Vector2(15,3.5f);
+
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //get current Horizontal Movement
+        float horDirection = Input.GetAxisRaw("Horizontal");
+        Vector3 horMove = horDirection * Vector3.right;
+        //posibilidad 1
+        /*if(horDirection > 0){
+
+        }else if(horDirection < 0){
+
+        }*/
+
+        //posibilidad2
+        /*if (transform.position.x < -limits.x || transform.position.x > limits.x){
+            horMove *= 0;
+            int limit = (int) transform.position.x;
+            Vector3 temp = transform.position;
+            temp.x = limit;
+            transform.position = temp;
+        }*/
+
+        //get current Vertical Movement
+        float verDirection = Input.GetAxisRaw("Vertical");
+        Vector3 verMove = verDirection * Vector3.up;
+
+        Vector3 temp = transform.position;
+        transform.Translate ((horMove + verMove).normalized * moveSpeed * Time.deltaTime);
+
+        //posibilidad3
+        /*if((transform.position.x < -limits.x || transform.position.x > limits.x) ||
+            (transform.position.y < -limits.y || transform.position.y > limits.y)){
+            transform.position = temp;
+        }*/
+
+        temp.x = Mathf.Clamp(transform.position.x,-limits.x,limits.x);
+        temp.y = Mathf.Clamp(transform.position.y,-limits.y,limits.y);
+        transform.position = temp;
+    }
+
+    void OnTriggerEnter2D (Collider2D other){
+        Debug.Log("Collision");
+    }
+    void OnDrawGizmos(){
+        Gizmos.DrawSphere(transform.position,0.15f);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(Vector3.zero,limits * 4);
+    }
+}
