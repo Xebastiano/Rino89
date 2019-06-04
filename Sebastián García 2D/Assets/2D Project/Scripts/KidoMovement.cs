daa8deef1004e4600360092c0cdf586cca61e48d
Xebastiano <sebastiangarciacastaneda@gmail.com>
2019-06-04 13:15:09 -0500

    Por si aca

--- Sebastián García 2D/Assets/2D Project/Scripts/KidoMovement.cs
+++ Sebastián García 2D/Assets/2D Project/Scripts/KidoMovement.cs
@@ -8,15 +8,23 @@ public class KidoMovement : MonoBehaviour{
     public MainScript Limits;
     Vector2 ColliderSize;
     Vector2 KidoLimits { get { return Limits.Limits - ((ColliderSize * transform.localScale) / 2); } }
-
+    Rigidbody2D KidoBody;
+    
     // Start is called before the first frame update
     void Start(){
         ColliderSize = gameObject.GetComponent<BoxCollider2D>().size;
+        KidoBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void FixedUpdate(){
         Vector2 verMove = Input.GetAxisRaw("Vertical") * Vector3.up;
         Vector2 horMove = Input.GetAxisRaw("Horizontal") * Vector3.right;
+        if (KidoBody){
+            Vector2 WalkingDirection = KidoBody.position + ((horMove + verMove).normalized * WalkingSpeed * Time.fixedDeltaTime);
+            WalkingDirection.x = Mathf.Clamp(WalkingDirection.x, -KidoLimits.x, KidoLimits.x);
+            WalkingDirection.y = Mathf.Clamp(WalkingDirection.y, -KidoLimits.y, KidoLimits.y);
+            KidoBody.MovePosition(WalkingDirection);
+        }
     }
 }
