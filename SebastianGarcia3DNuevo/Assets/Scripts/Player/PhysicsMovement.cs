bb39d468b040f452d36a975429d37adae90cb924
Xebastiano <sebastiangarciacastaneda@gmail.com>
2019-06-06 21:57:31 -0500

    Ponerse al dia

---
+++ SebastianGarcia3DNuevo/Assets/Scripts/Player/PhysicsMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsMovement : MonoBehaviour{
+
+    public float speed = 5;
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        Vector3 horizontal = Vector3.right * Input.GetAxis ("Horizontal");
+        transform.Translate (horizontal * speed * Time.deltaTime);
+    }
+}
