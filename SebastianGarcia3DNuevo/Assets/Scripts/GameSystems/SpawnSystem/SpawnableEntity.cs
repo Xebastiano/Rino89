a6dc5b2d6f7506f5f5ee72920cfa72d318a061a1
Xebastiano <sebastiangarciacastaneda@gmail.com>
2019-07-25 18:15:07 -0500

    Suuuuuu

--- SebastianGarcia3DNuevo/Assets/Scripts/GameSystems/SpawnSystem/SpawnableEntity.cs
+++ SebastianGarcia3DNuevo/Assets/Scripts/GameSystems/SpawnSystem/SpawnableEntity.cs
@@ -6,6 +6,14 @@ public class SpawnableEntity : MonoBehaviour {
 
     public string id;
 
+    void Start () {
+        Destroy (gameObject, 4);
+    }
+
+    void Update () {
+        transform.Translate (Vector3.forward * Time.deltaTime * 4);
+    }
+
     public void Create (Vector3 position, Quaternion rotation) {
         Instantiate (gameObject, position, rotation);
     }
--- SebastianGarcia3DNuevo/Assets/Scripts/GameSystems/SpawnSystem/Spawner.cs
+++ SebastianGarcia3DNuevo/Assets/Scripts/GameSystems/SpawnSystem/Spawner.cs
@@ -1,21 +1,35 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Linq;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour {
+
+    public TextAsset spawnData;
+    Queue<SpawnWave> spawnWaves;
 
     Queue<string> spawnQueue;
     int activeEntityLimit;
     public float spawnDelay;
     float currentTimer;
-
+    float currentDelta;
+    float variantTimer;
 
     // Start is called before the first frame update
     void Start () {
         spawnQueue = new Queue<string> ();
-        spawnQueue.Enqueue ("cube");
-        spawnQueue.Enqueue ("sphere");
+        spawnWaves = new Queue<SpawnWave> ();
+
+        if (spawnData) {
+            ParseData (spawnData.text);
+        }
+
+        Debug.Log (spawnWaves.Count + " Waves were created");
+        Debug.Log ("First wave has " + spawnWaves.Peek ().enemies.Length + " enemies");
+        Debug.Log ("First enemy is " + spawnWaves.Peek ().enemies[0]);
+
+        FillQueue ();
     }
 
     // Update is called once per frame
@@ -31,12 +45,48 @@ public class Spawner : MonoBehaviour {
         }
     }
 
+    void FillQueue () {
+        SpawnWave wave = spawnWaves.Dequeue ();
+        foreach (string enemy in wave.enemies) {
+            spawnQueue.Enqueue (enemy);
+        }
+        spawnDelay = wave.startTime;
+        currentDelta = wave.timeDelta;
+        variantTimer = wave.timer;
+    }
+
+    void ParseData (string textData) {
+        XDocument xmlData = XDocument.Parse (textData);
+
+        foreach (XElement element in xmlData.Root.Elements ()) {
+            float startTime = float.Parse (element.Attribute ("startTime").Value);
+            float timer = float.Parse (element.Attribute ("timer").Value);
+            float timeDelta = float.Parse (element.Attribute ("timeDelta").Value);
+            SpawnWave wave = new SpawnWave (startTime, timer, timeDelta);
+
+            List<string> enemies = new List<string> ();
+            foreach (XElement enemy in element.Element ("enemies").Elements ()) {
+                enemies.Add (enemy.Value);
+            }
+
+            wave.Populate (enemies.ToArray ());
+
+            spawnWaves.Enqueue (wave);
+        }
+    }
+
     void SpawnEntity () {
         if (spawnQueue.Count > 0) {
             SpawnableEntity next = SpawnManager.instance.Search (spawnQueue.Dequeue ());
             next.Create (transform.position, Quaternion.identity);
+            variantTimer += currentDelta;
+            spawnDelay = variantTimer;
+
+            if (spawnQueue.Count <= 0 && spawnWaves.Count > 0) {
+                FillQueue ();
+            }
         } else {
-            Debug.LogWarning ("Empty Queue");
+            Debug.LogWarning ("No more waves");
         }
     }
 
